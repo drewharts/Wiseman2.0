e23cbecf99c5dd7f1f52b16f3102978a3f860449
drewhartsfield <drewharts8@gmail.com>
2023-11-28 16:56:49 -0700

    added ArtistResponseComponent

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import TopArtistList from './components/TopArtistList'
 
 function App() {
   return (
     <ChakraProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route path="/" element={<TopArtistList />} />
         </Routes>
       </BrowserRouter>
     </ChakraProvider>
---
+++ frontend/src/components/TopArtistList.tsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { ArtistResponse } from '../models/ArtistResponse';
+
+const ArtistList = () => {
+  const [data, setData] = useState<ArtistResponse | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get<ArtistResponse>('http://ec2-54-196-229-34.compute-1.amazonaws.com:3000/my-top-artists');
+        setData(response.data);
+      } catch (err) {
+        setError('Error fetching data');
+        console.error(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
+
+  return (
+    <div>
+      {data?.items.map(artist => (
+        <div key={artist.id}>
+          <h2>{artist.name}</h2>
+          {/* Other artist details */}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ArtistList;
